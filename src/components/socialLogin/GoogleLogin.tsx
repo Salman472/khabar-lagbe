65556b6de9f22fe9c056b52430e0a7aee55c1e80
Salman472 <mds619820@gmail.com>
2026-04-11 12:18:47 +0600

    create google signin api and signup api

--- src/components/socialLogin/GoogleLogin.tsx
+++ src/components/socialLogin/GoogleLogin.tsx
@@ -1,24 +1,47 @@
-import Image from 'next/image';
-import React from 'react';
-import GoogleImage from '@/assets/google.png'
+import Image from "next/image";
+import React, { FormEvent, useState } from "react";
+import GoogleImage from "@/assets/google.png";
+import { signIn } from "next-auth/react";
+import { Loader2 } from "lucide-react";
 const GoogleLogin = () => {
-    return (
-        <div>
-            <button className="flex items-center justify-center gap-3 w-full border border-gray-300 rounded-xl py-3 px-4 hover:bg-gray-100 transition-all duration-200">
-      
-      <Image 
-        src={GoogleImage}
-        alt="google"
-        width={20}
-        height={20}
-      />
+  const [loading,setLoading]=useState(false)
+  const hangleGoogleButton=async(e:FormEvent)=>{
+    setLoading(true)
+    try {
+      await signIn('google')
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+    }
+  }
+  return (
+    <div>
+      <button
+      type="button"
+        onClick={hangleGoogleButton}
+        className="flex items-center justify-center gap-3 w-full border border-gray-300 rounded-xl py-3 px-4 hover:bg-gray-100 transition-all duration-200"
+      >
+        
 
-      <span className="text-sm font-medium text-gray-700">
-        Continue with Google
-      </span>
-    </button>
-        </div>
-    );
+        {/* <span className="text-sm font-medium text-gray-700"> */}
+          
+          {loading ? (
+            <>
+              <Loader2 className="h-5 w-5 animate-spin" />
+             <Image src={GoogleImage} alt="google" width={20} height={20} />
+              <span>Continue with Google</span>
+            </>
+          ) : (
+            <>
+             <Image src={GoogleImage} alt="google" width={20} height={20} />
+            <span>Continue with Google</span>
+            </>
+          )}
+        {/* </span> */}
+      </button>
+    </div>
+  );
 };
 
 export default GoogleLogin;
