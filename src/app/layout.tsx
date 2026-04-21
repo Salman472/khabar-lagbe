42dd6581473bd70ad34e70cc278098ed874b29e4
Salman472 <mds619820@gmail.com>
2026-04-21 20:06:57 +0600

    create usr slice

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Provider from "@/components/sessionProvider/Provider";
+import StoreProvider from "@/redux/StoreProvider";
 
 export const metadata: Metadata = {
   title: "Khabar Lagbe",
@@ -18,7 +19,9 @@ export default function RootLayout({
       className="w-full min-h-screen bg-linear-to-b from-green-50 to-white"
     >
       <body className="min-h-full flex flex-col">
-        <Provider>{children}</Provider>
+        <Provider>
+          <StoreProvider>{children}</StoreProvider>
+        </Provider>
       </body>
     </html>
   );
---
+++ src/redux/userSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+import mongoose from 'mongoose'
+
+interface IUser{
+    _id?:mongoose.Types.ObjectId
+    name:string
+    email:string
+    password?:string
+    mobile?:string
+    role:'user' | 'deliveryBoy' | 'admin'
+    image?:string
+}
+// Define a type for the slice state
+interface IUserSlice {
+  userData: IUser | null
+  
+}
+
+// Define the initial state using that type
+const initialState: IUserSlice = {
+  userData:null
+  
+}
+
+export const counterSlice = createSlice({
+  name: 'user',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    SetUserData:(state, action)=>{
+        state.userData=action.payload
+    }
+  },
+})
+
+export const { SetUserData } = counterSlice.actions
+
+export default counterSlice.reducer
