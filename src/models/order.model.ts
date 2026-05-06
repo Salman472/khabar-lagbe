07416b363fb2c6b1393fc286db2233a8bfd9caf7
Salman472 <mds619820@gmail.com>
2026-05-06 23:00:18 +0600

    order schema created

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,29 +1,77 @@
 import mongoose from "mongoose";
-
-interface IOrder{
-    _id?:mongoose.Types.ObjectId
-    user:mongoose.Types.ObjectId
-    items:[
-        {
-            grocery:mongoose.Types.ObjectId
-            name:string
-            price:string
-            unit:string
-            quantity:number
-        }
-    ]
-    totalAmount:string
-    paymentMethod:'cod' | 'online'
-    address:{
-        fullName:string
-        city:string
-        state:string
-        pinCode:string
-        fullAddress:string
-        latitude:number
-        longitude:number
-    }
-    status:'pending' | 'out of delivery' | 'delevered'
-    createdAt?:Date
-    updatedAt?:Date
+// order interface
+interface IOrder {
+  _id?: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  items: [
+    {
+      grocery: mongoose.Types.ObjectId;
+      name: string;
+      price: string;
+      unit: string;
+      quantity: number;
+    },
+  ];
+  totalAmount: string;
+  paymentMethod: "cod" | "online";
+  address: {
+    fullName: string;
+    city: string;
+    state: string;
+    pinCode: string;
+    fullAddress: string;
+    latitude: number;
+    longitude: number;
+  };
+  status: "pending" | "out of delivery" | "delevered";
+  createdAt?: Date;
+  updatedAt?: Date;
 }
+
+// order schema
+const orderSchema = new mongoose.Schema<IOrder>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        grocery: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Grocery",
+          required: true,
+        },
+
+        name: String,
+        price: String,
+        unit: String,
+        quantity: Number,
+      },
+    ],
+    totalAmount: {
+      type: String,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["cod", "online"],
+      default: "online",
+    },
+    address: {
+      fullName: String,
+      city: String,
+      state: String,
+      pinCode: String,
+      fullAddress: String,
+      latitude: Number,
+      longitude: Number
+    },
+    status:{
+        type:String,
+        enum:['pending','out of delivery', 'delivered'],
+        default:'pending'
+    }
+  },
+  { timestamps: true },
+);
