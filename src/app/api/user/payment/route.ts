042d274fbf2f753b19b79239f36261b55b2e47f1
Salman472 <mds619820@gmail.com>
2026-05-07 13:20:06 +0600

    stripe payment intrigate successful

--- src/app/api/user/payment/route.ts
+++ src/app/api/user/payment/route.ts
@@ -4,9 +4,8 @@ import User from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-
 // stripe instance
-const stripe=new Stripe(process.env.STRIPE_API_SECRET!)
+const stripe = new Stripe(process.env.STRIPE_API_SECRET!);
 export async function POST(req: NextRequest) {
   try {
     await connectDb();
@@ -35,6 +34,35 @@ export async function POST(req: NextRequest) {
 
     // stripe intrigate
 
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: `${process.env.NEXT_BASE_URL}/user/order-success`,
+      cancel_url: `${process.env.NEXT_BASE_URL}/user/order-cancel`,
+       line_items: [
+      {
+        price_data: {
+          currency: 'bdt',
+          product_data: {
+            name: 'Khabar Lagbe Order Payment',
+          },
+          unit_amount: totalAmount * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    metadata:{orderId:newOrder._id}
 
-  } catch (error) {}
+    });
+    return NextResponse.json(
+        {url:session.url},
+        {status:200}
+    )
+  } 
+  catch (error) {
+    return NextResponse.json(
+        {message:`stripe payment error ${error}`},
+        {status:500}
+    )
+  }
 }
