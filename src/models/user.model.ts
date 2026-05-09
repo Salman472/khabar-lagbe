0b4e0e6df7f061c6134978594ae58acf4b171b5b
Salman472 <mds619820@gmail.com>
2026-05-09 20:01:28 +0600

    update user modal and add socketId and isOnline

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,67 +1,78 @@
 import mongoose from "mongoose";
 
-interface IUser{
-    _id?:mongoose.Types.ObjectId
-    name:string
-    email:string
-    password?:string
-    mobile?:string
-    role:'user' | 'deliveryBoy' | 'admin'
-    image?:string
-    location: {
+interface IUser {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password?: string;
+  mobile?: string;
+  role: "user" | "deliveryBoy" | "admin";
+  image?: string;
+  socketId:string | null
+  isOnline:boolean
+  location: {
     type: {
-        type: StringConstructor;
-        enum: string[];
-        required: boolean;
-        default: string;
+      type: StringConstructor;
+      enum: string[];
+      required: boolean;
+      default: string;
     };
-    coordinates: {type: NumberConstructor[];
-        required: boolean;
-    };
+    coordinates: { type: NumberConstructor[]; required: boolean };
+  };
 }
-}
-const userSchema=new mongoose.Schema<IUser>({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        unique:true,
-        required:true
-    },
-    password:{
-        type:String,
-        required:false
-    },
-    mobile:{
-        type:String,
-        required:false
-    },
-    role:{
-        type:String,
-        enum:['user', 'deliveryBoy', 'admin'],
-        default:'user'
-    },
-    image:{
-        type:String,
-        required:false
-    },
-     location: {
-    type: {
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
       type: String,
-      enum: ["Point"],
-      
-      default: "Point",
+      required: true,
     },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    mobile: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "deliveryBoy", "admin"],
+      default: "user",
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    socketId:{
+        type:String,
+        default:null
+    },
+    isOnline:{
+        type:Boolean,
+        default:false
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
 
-    coordinates: {
-      type: [Number], 
-      default:[0,0]
+        default: "Point",
+      },
+
+      coordinates: {
+        type: [Number],
+        default: [0, 0],
+      },
     },
   },
-},{timestamps:true})
-userSchema.index({location:"2dsphere"})
-const User=mongoose.models.User || mongoose.model("User", userSchema)
+  { timestamps: true },
+);
+userSchema.index({ location: "2dsphere" });
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-export default User
+export default User;
